ddbb0dbfe42c9ba2df2200d063eab1ae4ce85f54
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-12-16 03:18:44 +0300

    initial commit

---
+++ src/main/java/com/ofg/elasticwithrdbms/ElasticwithrdbmsApplication.java
@@ -0,0 +1,11 @@
+package com.ofg.elasticwithrdbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticwithrdbmsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticwithrdbmsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ofg/elasticwithrdbms/ElasticwithrdbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofg.elasticwithrdbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticwithrdbmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
